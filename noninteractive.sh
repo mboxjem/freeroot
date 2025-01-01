19142a4184197f4bdc6f2dcda862437bd2c374b6
mboxjem <mboxjem@gmail.com>
2025-01-01 23:11:39 +0700

    Update noninteractive.sh

--- noninteractive.sh
+++ noninteractive.sh
@@ -1,86 +1,52 @@
 #!/bin/sh
 
-ROOTFS_DIR=$(pwd)
-export PATH=$PATH:~/.local/usr/bin
-max_retries=50
-timeout=1
-ARCH=$(uname -m)
+rootfs_dir=$(pwd)
+arch=$(uname -m)
+export PATH="$PATH:~/.local/usr/bin"
+MAX_RETRIES=50
+TIMEOUT=1
+UBUNTU_VERSION=22.04
 
-if [ "$ARCH" = "x86_64" ]; then
-  ARCH_ALT=amd64
-elif [ "$ARCH" = "aarch64" ]; then
-  ARCH_ALT=arm64
-else
-  printf "Unsupported CPU architecture: ${ARCH}"
-  exit 1
-fi
+install_ubuntu() {
+  wget --tries="$MAX_RETRIES" --timeout="$TIMEOUT" --no-hsts -O /tmp/rootfs.tar.gz \
+    "http://cdimage.ubuntu.com/ubuntu-base/releases/$UBUNTU_VERSION/release/ubuntu-base-$UBUNTU_VERSION.4-base-${arch_alt}.tar.gz"
+  tar -xf /tmp/rootfs.tar.gz -C "$rootfs_dir"
+}
 
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  echo "#######################################################################################"
-  echo "#"
-  echo "#                                      Foxytoux INSTALLER"
-  echo "#"
-  echo "#                           Copyright (C) 2024, RecodeStudios.Cloud"
-  echo "#"
-  echo "#"
-  echo "#######################################################################################"
+install_proot() {
+  mkdir -p "$rootfs_dir/usr/local/bin"
+  wget --tries="$MAX_RETRIES" --timeout="$TIMEOUT" --no-hsts -O "$rootfs_dir/usr/local/bin/proot" "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${arch}"
 
-  install_ubuntu=YES
-fi
-
-case $install_ubuntu in
-  [yY][eE][sS])
-    wget --tries=$max_retries --timeout=$timeout --no-hsts -O /tmp/rootfs.tar.gz \
-      "http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-${ARCH_ALT}.tar.gz"
-    tar -xf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
-    ;;
-  *)
-    echo "Skipping Ubuntu installation."
-    ;;
-esac
-
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  mkdir $ROOTFS_DIR/usr/local/bin -p
-  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
-
-  while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
-    rm $ROOTFS_DIR/usr/local/bin/proot -rf
-    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
-
-    if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
-      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-      break
-    fi
-
-    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+  while [ ! -s "$rootfs_dir/usr/local/bin/proot" ]; do
+    rm -rf "$rootfs_dir/usr/local/bin/proot"
+    wget --tries="$MAX_RETRIES" --timeout="$TIMEOUT" --no-hsts -O "$rootfs_dir/usr/local/bin/proot" "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${arch}"
     sleep 1
   done
 
-  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
-fi
-
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
-  rm -rf /tmp/rootfs.tar.xz /tmp/sbin
-  touch $ROOTFS_DIR/.installed
-fi
-
-CYAN='\e[0;36m'
-WHITE='\e[0;37m'
-
-RESET_COLOR='\e[0m'
-
-display_gg() {
-  echo -e "${WHITE}___________________________________________________${RESET_COLOR}"
-  echo -e ""
-  echo -e "           ${CYAN}-----> Mission Completed ! <----${RESET_COLOR}"
-  echo -e ""
-  echo -e "${WHITE}___________________________________________________${RESET_COLOR}"
+  chmod 755 "$rootfs_dir/usr/local/bin/proot"
 }
 
-clear
-display_gg
+configure_resolv() {
+  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4" > "$rootfs_dir/etc/resolv.conf"
+}
 
-$ROOTFS_DIR/usr/local/bin/proot \
-  --rootfs="${ROOTFS_DIR}" \
+if [ "$arch" = "x86_64" ]; then
+  arch_alt=amd64
+elif [ "$arch" = "aarch64" ]; then
+  arch_alt=arm64
+else
+  printf "Unsupported CPU architecture: %s\n" "$arch"
+  exit 1
+fi
+
+if [ ! -e "$rootfs_dir/.installed" ]; then
+  install_ubuntu
+  install_proot
+  configure_resolv
+  touch "$rootfs_dir/.installed"
+  printf "Mission Completed\n"
+fi
+
+"$rootfs_dir/usr/local/bin/proot" \
+  --rootfs="$rootfs_dir" \
   -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
